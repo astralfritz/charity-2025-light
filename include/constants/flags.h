f9a505fb8a860a64160461b7266ce25de1c6e897
Fritz <jfkern01@hotmail.com>
2026-08-03 12:31:38 -0400

    adding some items to the houses

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -670,8 +670,8 @@
 #define FLAG_ITEM_CHARITY_HALLWAY2_4  0x26F // Unused Flag
 #define FLAG_ITEM_CHARITY_HALLWAY2_5  0x270 // Unused Flag
 #define FLAG_ITEM_CHARITY_HOUSE_2_1  0x271 // Unused Flag
-#define FLAG_UNUSED_0x272  0x272 // Unused Flag
-#define FLAG_UNUSED_0x273  0x273 // Unused Flag
+#define FLAG_HIDDEN_ITEM_CHARITY_HOUSE_1_FULL_HEAL  0x272 // Unused Flag
+#define FLAG_HIDDEN_ITEM_CHARITY_HOUSE_1_SODA_POP  0x273 // Unused Flag
 #define FLAG_UNUSED_0x274  0x274 // Unused Flag
 #define FLAG_UNUSED_0x275  0x275 // Unused Flag
 #define FLAG_UNUSED_0x276  0x276 // Unused Flag
